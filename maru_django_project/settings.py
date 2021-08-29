cec31ed197eeac49c53487e25771f0fd1d28b892
Mia Sibala <miluchia.sibala@gmail.com>
2021-08-29 12:52:07 -0700

    added mime type

--- maru_django_project/settings.py
+++ maru_django_project/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import mimetypes
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,6 +132,7 @@ STATICFILES_DIRS = (
      os.path.join(BASE_DIR, 'apps/maru_app/static'),
 )
 
+mimetypes.add_type("text/css", ".css", True)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
